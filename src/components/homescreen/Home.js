132d5153cf3ebca4c5d9401853ee67fad6e2a1ce
Kharakkular <Kharakkular2811@gmail.com>
2022-07-18 16:07:12 -0400

    Initialised the calculator project with npx-create-react-app

---
+++ src/components/homescreen/Home.js
@@ -0,0 +1,15 @@
+// import { Fragment } from "react";
+
+import classes from './Home.module.css';
+
+const Home = (props) => {
+    return (
+        <div className={classes.container}>
+            <div className={classes.main}>
+                <h3>Calculator</h3>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
